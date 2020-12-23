ea467a0f11ab4e579e2bb2d0a81269a3b7955da7
Maxime ALFOSEA <maxime.alfosea@hotmail.fr>
2020-12-23 19:45:35 +0100

    Ajoutez des fichiers projet.

---
+++ madera-api/Enums/CivilityEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace madera_api.Enums
+{
+    public enum CivilityEnum
+    {
+        UKNOWN = 0,
+        MAN = 1,
+        WOMAN = 2
+    }
+}
